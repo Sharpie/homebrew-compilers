b681eb7205ff7e7c2e945e52d3b02fcb803f2bfe
rhysd <u033263f@ecs.cmc.osaka-u.ac.jp>
2012-03-26 01:25:31 +0900

    GCC 4.7.0
    Closes #3.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- gcc.rb
+++ gcc.rb
@@ -45,9 +45,9 @@ end
 
 class Gcc < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftpmirror.gnu.org/gcc/gcc-4.6.3/gcc-4.6.3.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.6.3/gcc-4.6.3.tar.bz2'
-  md5 '773092fe5194353b02bb0110052a972e'
+  url 'http://ftpmirror.gnu.org/gcc/gcc-4.7.0/gcc-4.7.0.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.7.0/gcc-4.7.0.tar.bz2'
+  md5 '2a0f1d99fda235c29d40b561f81d9a77'
 
   depends_on 'gmp'
   depends_on 'libmpc'
